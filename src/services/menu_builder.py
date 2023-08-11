da23c3d93e6dd484db41c809af0d242f5ed6099e
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-08-11 13:44:38 -0300

    :sparkles: req4
      Implementei o método get_main_menu dentro da classe MenuBuilder, que gera um DataFrame com os cardápios. Ele se encontra no arquivo src/services/menu_builder.py.

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -1,29 +1,29 @@
-from typing import Dict, List
-
 from services.inventory_control import InventoryMapping
 from services.menu_data import MenuData
 
-DATA_PATH = "data/menu_base_data.csv"
-INVENTORY_PATH = "data/inventory_base_data.csv"
-
 
 class MenuBuilder:
-    def __init__(self, data_path=DATA_PATH, inventory_path=INVENTORY_PATH):
+    def __init__(self, data_path, inventory_path):
         self.menu_data = MenuData(data_path)
         self.inventory = InventoryMapping(inventory_path)
 
-    def make_order(self, dish_name: str) -> None:
-        try:
-            curr_dish = [
-                dish
-                for dish in self.menu_data.dishes
-                if dish.name == dish_name
-            ][0]
-        except IndexError:
-            raise ValueError("Dish does not exist")
+    def get_main_menu(self, restriction=None):
+        def is_dish_available(dish):
+            return (
+                not (restriction and restriction in dish.get_restrictions())
+                and all(self.inventory.inventory.get(ingredient, 0) > 0 for ingredient in dish.get_ingredients())
+            )
 
-        self.inventory.consume_recipe(curr_dish.recipe)
+        menu = [
+            {
+                "dish_name": prato.name,
+                "ingredients": prato.get_ingredients(),
+                "price": prato.price,
+                "restrictions": prato.get_restrictions(),
+            }
+            for prato in self.menu_data.dishes
+            if is_dish_available(prato)
+        ]
 
-    # Req 4
-    def get_main_menu(self, restriction=None) -> List[Dict]:
-        pass
+        return menu
+
