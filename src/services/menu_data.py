1e96293bb5716b8385ee918528b60c78b723aba0
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-08-11 12:57:11 -0300

    :sparkles: req3
     Mapeamento Pratos <> Ingredientes

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,33 @@
-# Req 3
+import csv
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        with open(source_path, "r") as file:
+            reader = csv.DictReader(file)
+            dish_dict = {}
+            ingredient_dict = {}
+
+            for row in reader:
+                dish_name = row["dish"]
+                price = float(row["price"])
+                ingredient_name = row["ingredient"]
+                recipe_amount = int(row["recipe_amount"])
+
+                if dish_name not in dish_dict:
+                    dish = Dish(dish_name, price)
+                    dish_dict[dish_name] = dish
+                else:
+                    dish = dish_dict[dish_name]
+
+                if ingredient_name not in ingredient_dict:
+                    ingredient = Ingredient(ingredient_name)
+                    ingredient_dict[ingredient_name] = ingredient
+                else:
+                    ingredient = ingredient_dict[ingredient_name]
+
+                dish.add_ingredient_dependency(ingredient, recipe_amount)
+
+        self.dishes = set(dish_dict.values())
